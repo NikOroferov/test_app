a60d447e75ceb6b59a3ae5415e8a4cba6a4f406b
Nikolay Yaryhin <77698141+NikOroferov@users.noreply.github.com>
2023-03-13 12:22:56 +0200

    Add: change page, sessionStorage

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -1,19 +1,15 @@
 import styles from "@/styles/Question.module.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const quizQuestions = [
   {
     id: 1,
     question: "Are you old?",
-    answers: ["Yep", "Nope", "Maybe"],
+    answers: ["0-6", "7-8", "9-63"],
   },
   {
     id: 2,
-    question: "Do you like chocolate?",
-    answers: ["Yep", "Nope", "Maybe"],
-  },
-  {
-    id: 3,
     question: "Which you favorite color",
     answers: [
       "Gray",
@@ -27,6 +23,11 @@ const quizQuestions = [
     ],
   },
   {
+    id: 3,
+    question: "Do you like chocolate?",
+    answers: ["Yep", "Nope", "Maybe"],
+  },
+  {
     id: 4,
     question: "What do you think about sun?",
     answers: ["It's funny", "It's brutal", "It's wrong"],
@@ -34,45 +35,71 @@ const quizQuestions = [
   {
     id: 5,
     question: "Do you beautiful?",
-    answers: ["Yep", "Nope", "My mom says yes"],
+    answers: ["My mom says yes :D", "Nobody says yes :'("],
   },
 ];
 
 export default function Question() {
-  const [question, setQuestion] = useState(quizQuestions[0]);
-  const [answer, setAnswer] = useState(null);
+  const [quizQuestionItem, setQuizQuestionItem] = useState(quizQuestions[0]);
+  const [userAnswer, setUserAnswer] = useState(null);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [questionNumber, setQuestionNumber] = useState(0);
 
   const handleChange = event => {
-    console.log(event.target);
+    const answer = {
+      id: nanoid(),
+      question: setQuizQuestionItem.question,
+      answer: event.target.value,
+    };
+
+    setUserAnswer(answer);
   };
 
-  const handleSubmit = () => {
-    console.log(question);
+  const handleNextButton = event => {
+    event.preventDefault();
+
+    if (!userAnswer) {
+      alert("Choose an answer");
+      return;
+    }
+
+    const answers = [...userAnswers, userAnswer];
+    setUserAnswers(answers);
+    sessionStorage.setItem("user-answers", JSON.stringify(answers));
+
+    let number = questionNumber + 1;
+    if (number >= quizQuestions.length) {
+      number = 0;
+    }
+
+    setQuestionNumber(number);
+    setQuizQuestionItem(quizQuestions[number]);
+    setUserAnswer(null);
   };
 
   return (
     <section className={styles.section}>
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form}>
         <fieldset className={styles.fieldset}>
-          <legend className={styles.legend}>{question.question}</legend>
-          {question ? (
-            question.answers.map(answer => (
-              <label key={answer}>
-                <input
-                  type="radio"
-                  className={styles.input}
-                  name="answer"
-                  value={answer}
-                  onChange={handleChange}
-                />
-                {answer}
-              </label>
-            ))
-          ) : (
-            <p>Something went wrong</p>
-          )}
+          <legend className={styles.legend}>{quizQuestionItem.question}</legend>
+          {quizQuestionItem.answers.map(answer => (
+            <label key={answer} className={styles.label}>
+              <input
+                type="radio"
+                className={styles.input}
+                name="answer"
+                value={answer}
+                onChange={handleChange}
+              />
+              {answer}
+            </label>
+          ))}
         </fieldset>
-        <button type="submit" className={styles.button}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={handleNextButton}
+        >
           Next
         </button>
       </form>
